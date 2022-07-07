ed38b37d627a82087e726dc7af424d309c9790c1
HoldenClark <holdenjclark@gmail.com>
2022-07-07 10:53:40 -0400

    Madlibs
    simple small madlib to demonstrate I can upload a python program to github

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+#creating a madlib using python and my basic knowledge
+
+
+#madlibs variable value
+
+verb1 = input("Enter first verb here: ")
+verb2 = input("Enter second verb here: ")
+noun1 = input("Enter first noun here: ")
+noun2 = input("Enter second noun here: ")
+adj1 = input("Enter first adjective here: ")
+
+
+#start of the mad lib
+madlib = (f"I would like to {verb1} python because it will {verb2} me. I think"\
+f" {noun1} will help me with my {noun2} and make me {adj1} when I complete it.")
+
+print(madlib)
